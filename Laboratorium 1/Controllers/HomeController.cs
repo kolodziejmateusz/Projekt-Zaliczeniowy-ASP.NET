3643f30078d2790183456d0caaa5a08128aab51e
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2024-02-19 16:41:10 +0100

    Pierwszy kontroler i widok

--- Laboratorium 1/Controllers/HomeController.cs
+++ Laboratorium 1/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace Laboratorium_1.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult About()
+        {
+            return View();
+        }
     }
 }
