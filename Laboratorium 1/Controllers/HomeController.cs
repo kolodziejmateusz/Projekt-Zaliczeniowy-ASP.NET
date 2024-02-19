3bd9bf32c448ceac451c8a04e8e378595ecf3327
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2024-02-19 17:15:38 +0100

    Lab1-Zadanie3-gotowe-gotowy kalkulator z logiką

--- Laboratorium 1/Controllers/HomeController.cs
+++ Laboratorium 1/Controllers/HomeController.cs
@@ -44,6 +44,39 @@ namespace Laboratorium_1.Controllers
             ViewBag.Op = op;
             ViewBag.A = a;
             ViewBag.B = b;
+
+            if (a ==  null || b == 0 || op == Operator.Unknown) 
+            {
+                ViewBag.Result = "Nie podałeś jakiegoś składnika ";
+            }
+            else
+            {
+                if (op == Operator.Add)
+                {
+                    ViewBag.Result = a + b;
+                }
+                else if (op == Operator.Mul)
+                {
+                    ViewBag.Result = a * b;
+                }
+                else if (op == Operator.Sub)
+                {
+                    ViewBag.Result = a - b;
+                }
+                else if (op == Operator.Div)
+                {
+                    if (b == 0)
+                    {
+                        ViewBag.Result = "Nie wolno dzielić przez 0!";
+                    }
+                    else
+                    {
+                        ViewBag.Result = a / b;
+                    }
+                }
+            }
+
+
             return View();
         }
     }
