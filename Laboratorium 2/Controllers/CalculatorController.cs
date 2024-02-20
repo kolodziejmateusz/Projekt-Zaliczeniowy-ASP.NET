02c59e193a5fdc88bc515dbc6a01fbc28550b3fe
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2024-02-20 12:42:36 +0100

    Lab2-kalkulator, formularz z polami a, op, b

---
+++ Laboratorium 2/Controllers/CalculatorController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Laboratorium_2.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public enum Operator
+        {
+            Unknown, Add, Mul, Sub, Div
+        }
+        public IActionResult Result(Operator op, double a, double b)
+        {
+            ViewBag.Op = op;
+            ViewBag.A = a;
+            ViewBag.B = b;
+
+            if (a == null || b == 0 || op == Operator.Unknown)
+            {
+                ViewBag.Result = "Nie podałeś jakiegoś składnika ";
+            }
+            else
+            {
+                if (op == Operator.Add)
+                {
+                    ViewBag.Result = a + b;
+                }
+                else if (op == Operator.Mul)
+                {
+                    ViewBag.Result = a * b;
+                }
+                else if (op == Operator.Sub)
+                {
+                    ViewBag.Result = a - b;
+                }
+                else if (op == Operator.Div)
+                {
+                    if (b == 0)
+                    {
+                        ViewBag.Result = "Nie wolno dzielić przez 0!";
+                    }
+                    else
+                    {
+                        ViewBag.Result = a / b;
+                    }
+                }
+            }
+
+            return View();
+        }
+
+        public IActionResult Form()
+        {
+            return View();
+        }
+    }
+}
