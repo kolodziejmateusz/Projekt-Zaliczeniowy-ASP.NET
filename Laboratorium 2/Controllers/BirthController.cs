145342df0fb765bf8f60ea33594e78eb6d01b214
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2024-02-20 12:42:36 +0100

    Lab2-calosc-gotowe

---
+++ Laboratorium 2/Controllers/BirthController.cs
@@ -0,0 +1,22 @@
+﻿using Laboratorium_2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Laboratorium_2.Controllers
+{
+    public class BirthController : Controller
+    {
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        public IActionResult Result([FromForm] Birth birth)
+        {
+            if (!birth.IsValid()) 
+            {
+                return View("Error");
+            }
+            return View(birth);
+        }
+    }
+}
---
+++ Laboratorium 2/Models/Birth.cs
@@ -0,0 +1,21 @@
+﻿namespace Laboratorium_2.Models
+{
+    public class Birth
+    {
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public int Age(DateTime DateOfBirth)
+        {
+            var today = DateTime.Today;
+            var age = today.Year - DateOfBirth.Year;
+            if (DateOfBirth.Date > today.AddYears(-age)) age--;
+            return age;
+        }
+
+        public bool IsValid()
+        {
+            return Name != null && DateOfBirth < DateTime.Today;
+        }
+    }
+}
