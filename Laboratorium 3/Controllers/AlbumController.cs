c1c60c900c647eabb39d4a10eecef720a5fc2b89
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2024-02-22 14:26:56 +0100

    Lab3-calosc-gotowe-formularz z walidacją zapisujący do Dictonary i wyświetlanie na index listy Albumów z przyciskami Edytuj, Szczegóły, Usuń

---
+++ Laboratorium 3/Controllers/AlbumController.cs
@@ -0,0 +1,83 @@
+﻿using Laboratorium_3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Laboratorium_3.Controllers
+{
+    public class AlbumController : Controller
+    {
+        private static Dictionary<int, Album> _albums = new Dictionary<int, Album>();
+
+        public IActionResult Index()
+        {
+            return View(_albums);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Album album)
+        {
+            if (ModelState.IsValid)
+            {
+                int id = _albums.Keys.Count != 0 ? _albums.Keys.Max() : 0;
+                album.Id = id + 1;
+                _albums.Add(album.Id, album);
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(album);
+            }
+        }
+
+        //[HttpGet]
+        //public String Edit(int? id)
+        //{
+        //    return "Edycja " + id;
+        //}
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            if (_albums.Keys.Contains(id))
+            {
+                return View(_albums[id]);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            if (_albums.Keys.Contains(id))
+            {
+                return View(_albums[id]);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            if (_albums.Keys.Contains(id))
+            {
+                return View(_albums[id]);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
