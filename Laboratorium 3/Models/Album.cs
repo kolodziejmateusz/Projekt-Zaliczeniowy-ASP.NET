b956ac14183eed07b648ec74505a04196a5dbe36
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2024-02-22 14:26:56 +0100

    Lab3-Zadanie1-gotowe-Klasa Album z atrybutami walidującymi

---
+++ Laboratorium 3/Models/Album.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Laboratorium_3.Models
+{
+    public class Album
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Podaj nazwe albumu.")]
+        [MaxLength(100, ErrorMessage = "Nazwa albumu jest za długa"), MinLength(3, ErrorMessage = "Nazwa albumu jest za któtka.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Podaj nazwe zespołu.")]
+        [MaxLength(100, ErrorMessage = "Nazwa zespołu jest za długa"), MinLength(3, ErrorMessage = "Nazwa zespołu jest za któtka.")]
+        public string Band { get; set; }
+        public List<string>? TrackList { get; set; }
+        [Required(ErrorMessage = "Podaj date wydania.")]
+        public DateTime ReleaseDate { get; set; }
+        [Required(ErrorMessage = "Podaj czas trwania.")]
+        public TimeSpan Duration { get; set; }
+    }
+}
