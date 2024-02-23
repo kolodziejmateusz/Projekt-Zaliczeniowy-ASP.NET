75a31920a7bfa11b71ab67e499a02f9c2aefe072
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2024-02-23 11:43:47 +0100

    Lab4-Dodanie seedowania dwóch przykładowych albumów

--- Laboratorium 4/Controllers/AlbumController.cs
+++ Laboratorium 4/Controllers/AlbumController.cs
@@ -1,4 +1,5 @@
 ﻿using Laboratorium_3.Models;
+using Laboratorium_4.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Laboratorium_3.Controllers
@@ -6,6 +7,35 @@ namespace Laboratorium_3.Controllers
     public class AlbumController : Controller
     {
         private static Dictionary<int, Album> _albums = new Dictionary<int, Album>();
+        static AlbumController()
+        {
+            var album1 = new Album
+            {
+                Id = 1,
+                Name = "Abbey Road",
+                Band = "The Beatles",
+                TrackList = "Come Together, Something, Maxwell's Silver Hammer, Oh! Darling, Octopus's Garden, I Want You (She's So Heavy), Here Comes The Sun, Because, You Never Give Me Your Money, Sun King, Mean Mr. Mustard, Polythene Pam, She Came In Through The Bathroom Window, Golden Slumbers, Carry That Weight, The End, Her Majesty.",
+                ReleaseDate = new DateTime(1969, 9, 26),
+                Duration = "47:23",
+                Popularity = Popularity.High // Zakładając, że masz taką wartość w enumie Popularity
+            };
+
+            var album2 = new Album
+            {
+                Id = 2,
+                Name = "The Dark Side of the Moon",
+                Band = "Pink Floyd",
+                TrackList = "Speak to Me, Breathe, On the Run, Time, The Great Gig in the Sky, Money, Us and Them, Any Colour You Like, Brain Damage, Eclipse.",
+                ReleaseDate = new DateTime(1973, 3, 1),
+                Duration = "42:49",
+                Popularity = Popularity.TopChart
+            };
+
+            _albums.Add(album1.Id, album1);
+            _albums.Add(album2.Id, album2);
+        }
+
+
 
         public IActionResult Index()
         {
