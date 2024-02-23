4c826d6fec1a44849616e02b4a0351bd56c84ea7
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2024-02-23 12:14:28 +0100

    Lab4-Dodanie atrybutu Display do klasy Album

--- Laboratorium 4/Models/Album.cs
+++ Laboratorium 4/Models/Album.cs
@@ -9,15 +9,20 @@ namespace Laboratorium_3.Models
         [HiddenInput]
         public int Id { get; set; }
         [Required(ErrorMessage = "Podaj nazwe albumu.")]
+        [Display(Name = "Nazwa")]
         [MaxLength(100, ErrorMessage = "Nazwa albumu jest za długa"), MinLength(3, ErrorMessage = "Nazwa albumu jest za któtka.")]
         public string Name { get; set; }
         [Required(ErrorMessage = "Podaj nazwe zespołu.")]
+        [Display(Name = "Zespół")]
         [MaxLength(100, ErrorMessage = "Nazwa zespołu jest za długa"), MinLength(3, ErrorMessage = "Nazwa zespołu jest za któtka.")]
         public string Band { get; set; }
+        [Display(Name = "Lista piosenek")]
         public string? TrackList { get; set; }
         [Required(ErrorMessage = "Podaj date wydania.")]
+        [Display(Name = "Data wydania")]
         public DateTime ReleaseDate { get; set; }
         [Required(ErrorMessage = "Podaj czas trwania.")]
+        [Display(Name = "Czas trwania")]
         [RegularExpression(@"^([0-9]|[0-5][0-9]):[0-5][0-9]$", ErrorMessage = "Czas trwania musi być w formacie minuty i sekundy, 00:00 lub 0:00.")]
         public string Duration { get; set; }
         [Display(Name = "Popularność albumu")]
