51a4af27a563714dfa5b93baa12e240f06592d62
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2024-02-24 13:00:41 +0100

    Lab4-Dodanie serwisu Album

---
+++ Laboratorium 4/Models/IAlbumService.cs
@@ -0,0 +1,11 @@
+﻿namespace Laboratorium_4.Models
+{
+    public interface IAlbumService
+    {
+        int Add(Album album);
+        void Delete(int id);
+        void Update(Album albm);
+        List<Album> FindAll();
+        Album? FindById(int id);
+    }
+}
---
+++ Laboratorium 4/Models/MemoryAlbumService.cs
@@ -0,0 +1,35 @@
+﻿namespace Laboratorium_4.Models
+{
+    public class MemoryAlbumService : IAlbumService
+    {
+        private Dictionary<int, Album> _items = new Dictionary<int, Album>();
+
+        public int Add(Album item)
+        {
+            int id = _items.Keys.Count != 0 ? _items.Keys.Max() : 0;
+            item.Id = id + 1;
+            _items.Add(item.Id, item);
+            return item.Id;
+        }
+
+        public void Delete(int id)
+        {
+            _items.Remove(id);
+        }
+
+        public List<Album> FindAll()
+        {
+            return _items.Values.ToList();
+        }
+
+        public Album? FindById(int id) 
+        {
+            return _items[id];
+        }
+
+        public void Update(Album item)
+        {
+            _items[item.Id] = item;
+        }
+    }
+}
