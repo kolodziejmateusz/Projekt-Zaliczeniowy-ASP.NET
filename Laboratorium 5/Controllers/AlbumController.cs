50439a875e6253566e755c2639ec0cab68be58be
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2024-02-26 15:17:06 +0100

    Lab5 init

---
+++ Laboratorium 5/Controllers/AlbumController.cs
@@ -0,0 +1,79 @@
+﻿using Laboratorium_5.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Laboratorium_5.Controllers
+{
+    public class AlbumController : Controller
+    {
+        private readonly IAlbumService _albumService;
+
+        public AlbumController(IAlbumService albumService)
+        {
+            _albumService = albumService;
+        }
+
+        public IActionResult Index()
+        {
+            var albums = _albumService.FindAll();
+            return View(albums);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Album album)
+        {
+            if (ModelState.IsValid)
+            {
+                _albumService.Add(album);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(album);
+            }
+        }
+
+        public IActionResult Delete(int id)
+        {
+            _albumService.Delete(id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var album = _albumService.FindById(id);
+            if (album == null)
+            {
+                return NotFound();
+            }
+            return View(album);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Album album)
+        {
+            if (ModelState.IsValid)
+            {
+                _albumService.Edit(album);
+                return RedirectToAction("Index");
+            }
+            return View(album);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var album = _albumService.FindById(id);
+            if (album == null)
+            {
+                return NotFound();
+            }
+            return View(album);
+        }
+    }
+}
---
+++ Laboratorium 5/Models/IAlbumService.cs
@@ -0,0 +1,11 @@
+﻿namespace Laboratorium_5.Models
+{
+    public interface IAlbumService
+    {
+        int Add(Album album);
+        void Delete(int id);
+        void Edit(Album albm);
+        List<Album> FindAll();
+        Album? FindById(int id);
+    }
+}
