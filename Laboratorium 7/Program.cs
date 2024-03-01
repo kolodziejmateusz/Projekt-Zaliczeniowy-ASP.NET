d31724e0ac4e411687f1070ea1a75634fdc7aab5
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2024-03-01 15:07:21 +0100

    Lab7-Dodanie Identity

--- Laboratorium 7/Program.cs
+++ Laboratorium 7/Program.cs
@@ -1,4 +1,7 @@
 using Laboratorium_7.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Data;
 
 namespace Laboratorium_7
 {
@@ -7,11 +10,14 @@ namespace Laboratorium_7
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            var connectionString = builder.Configuration.GetConnectionString("AppDbContextConnection") ?? throw new InvalidOperationException("Connection string 'AppDbContextConnection' not found.");
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
             //builder.Services.AddSingleton<IAlbumService, MemoryAlbumService>();
             builder.Services.AddDbContext<Data.AppDbContext>();
+
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<AppDbContext>();
             builder.Services.AddTransient<IAlbumService, EFAlbumService>();
 
             var app = builder.Build();
