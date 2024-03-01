c1afa7a10de891c9a05bd6639d6272be8b7b299c
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2024-03-01 10:42:59 +0100

    Lab7 init

---
+++ Laboratorium 7/Controllers/AlbumController.cs
@@ -0,0 +1,92 @@
+﻿using Laboratorium_7.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Laboratorium_7.Controllers
+{
+    public class AlbumController : Controller
+    {
+        private readonly IAlbumService _albumService;
+
+        public AlbumController(IAlbumService albumService)
+        {
+            _albumService = albumService;
+        }
+
+        public IActionResult Index()
+        {
+            var albums = _albumService.FindAll();
+            return View(albums);
+        }
+
+        private void InitializeRecordLabels(Album model)
+        {
+            model.RecordLabels = _albumService
+                .FindAllOrganizations()
+                .Select(o => new SelectListItem { Value = o.Id.ToString(), Text = o.Name })
+                .ToList();
+        }
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            Album model = new Album();
+            InitializeRecordLabels(model);
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Album album)
+        {
+            if (ModelState.IsValid)
+            {
+                _albumService.Add(album);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                InitializeRecordLabels(album);
+                return View(album);
+            }
+        }
+
+        public IActionResult Delete(int id)
+        {
+            _albumService.Delete(id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var album = _albumService.FindById(id);
+            if (album == null)
+            {
+                return NotFound();
+            }
+            return View(album);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Album album)
+        {
+            if (ModelState.IsValid)
+            {
+                _albumService.Edit(album);
+                return RedirectToAction("Index");
+            }
+            return View(album);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var album = _albumService.FindById(id);
+            if (album == null)
+            {
+                return NotFound();
+            }
+            return View(album);
+        }
+    }
+}
