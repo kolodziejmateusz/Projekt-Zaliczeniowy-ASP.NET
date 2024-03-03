d9798692335e202d76d4a8e4e85ddf7eebcf0c8b
Mateusz Kolodziej <mateuszkolodziejti@gmail.com>
2024-03-03 17:55:40 +0100

    Lab8-RecordLabelApiController

---
+++ Laboratorium 8/Controllers/RecordLabelApiController.cs
@@ -0,0 +1,26 @@
+﻿using Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Laboratorium_8.Controllers
+{
+    [Route("api/recordlabels")]
+    [ApiController]
+    public class RecordLabelApiController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public RecordLabelApiController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetFiltered(string filter)
+        {
+            return Ok(_context.RecordLabels
+                .Where(o => o.Name.StartsWith(filter))
+                .Select(o => new { o.Name, o.Id })
+                .ToList());
+        }
+    }
+}
